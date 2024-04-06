ff594f250ce0c75b61e374f0d6fc0b580317ac31
yexuanyang <1182282462@qq.com>
2024-04-06 20:36:41 +0800

    experiment

--- r4l_experiment/driver/003_completion_rust/completion.rs
+++ r4l_experiment/driver/003_completion_rust/completion.rs
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
 
 //! Rust completion device
-
+use core::ops::Deref;
 use core::cell::UnsafeCell;
-// use core::ptr::null;
 
-// use kernel::PointerWrapper;
 use kernel::prelude::*;
 use kernel::{chrdev, file::{self, File}, bindings};
-use kernel::chrdev::Registration;
 use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
+use kernel::sync::Mutex;
+use kernel::task::Task;
 
 module! {
     type: CompletionDev,
@@ -19,96 +18,77 @@ module! {
     license: "GPL",
 }
 
-struct Completion(UnsafeCell<bindings::completion>);
+struct Completion(pub(crate) UnsafeCell<bindings::completion>);
 
-// static mut COMPLETION: *const Completion = null();
+static COMPLETION: Mutex<Option<Completion>> = unsafe {
+    Mutex::new(None)
+};
 
-unsafe impl Sync for Completion {}
+struct CompletionRust {}
+
 unsafe impl Send for Completion {}
-
-impl Completion {
-    /// Create a new Completion and init it
-    pub(crate) fn new() -> Self {
-        let completion = Completion(UnsafeCell::new(bindings::completion::default()));
-        unsafe{ bindings::init_completion(completion.0.get()); }
-        completion
-    }
-
-    /// Wait for a completion
-    pub(crate) fn wait_for_completion(&self) {
-        pr_info!("wait_for_completion: ptr address is {:?}", self.0.get());
-        unsafe{ bindings::wait_for_completion(self.0.get() ) }
-    }
-
-    /// Complete a completion
-    pub(crate) fn complete(&self) {
-        pr_info!("complete: ptr address is {:?}", self.0.get());
-        unsafe{ bindings::complete(self.0.get()) }
-    }
-}
 
 struct CompletionDev {
     _dev: Pin<Box<chrdev::Registration<1>>>,
 }
 
 #[vtable]
-impl file::Operations for Completion {
-    type Data = Box<Completion>;
+impl file::Operations for CompletionRust {
+    type Data = ();
 
-    fn open(_context: &(), _file: &File) -> Result<Box<Self>> {
-        pr_info!("open completion device");
-        // pr_info!("COMPLETION is {:?}", COMPLETION);
-        // unsafe {
-        //     if COMPLETION.is_null() {
-        //         pr_info!("null");
-        //         let box_completion = Box::try_new(Completion::new())?;
-        //         COMPLETION = box_completion.as_ref() as *const _;
-        //         pr_info!("after modify COMPLETION is {:?}", COMPLETION);
-        //         Ok(box_completion)
-
-        //     } else {
-        //         pr_info!("no null");
-        //         Ok(Box::from_pointer(COMPLETION as _))
-        //     }
-        // }
-        Ok(
-            Box::try_new(Completion::new())?
-        )
+    fn open(_context: &(), _file: &File) -> Result<()> {
+        pr_info!("function open is invoked");
+        Ok(())
     }
 
     fn read(
-        data: &Self,
+        _data: (),
         _file: &File,
         _writer: &mut impl IoBufferWriter,
         _offset: u64,
     ) -> Result<usize> {
         pr_info!("function read is invoked");
-        pr_info!("read ptr addr: {:?}", data as *const _);
-        data.wait_for_completion();
+        pr_info!("process {} is going to sleep", Task::current().pid());
+        let completion:*mut bindings::completion;
+        {
+            let lock = COMPLETION.lock();
+            completion = lock.deref().as_ref().unwrap().0.get();
+        }
+        unsafe { bindings::wait_for_completion(completion) };
+        pr_info!("awoken {}", Task::current().pid());
+        
         Ok(0)
     }
 
     fn write(
-        data: &Self,
+        _data: (),
         _file: &File,
         reader: &mut impl IoBufferReader,
         _offset: u64,
     ) -> Result<usize> {
         pr_info!("function write is invoked");
-        pr_info!("write ptr addr: {:?}", data as *const _);
-        data.complete();
+        pr_info!("process {} awakening the readers", Task::current().pid());
+        let completion:*mut bindings::completion;
+        {
+            let lock = COMPLETION.lock();
+            completion = lock.deref().as_ref().unwrap().0.get()
+        }
+        unsafe { bindings::complete(completion) };
         Ok(reader.len())
     }
 }
 
-
-
 impl kernel::Module for CompletionDev {
     fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
         pr_info!("init completion device");
-        let mut chrdev_reg = Registration::new_pinned(name, 0, module)?;
+        let mut chrdev_reg = chrdev::Registration::new_pinned(name, 0, module)?;
         
-        chrdev_reg.as_mut().register::<Completion>()?;
+        let mut completion = COMPLETION.lock();
+        *completion = Some(Completion(UnsafeCell::new(bindings::completion::default())));
+        let a = completion.deref().as_ref().unwrap().0.get();
+        unsafe{ bindings::init_completion(a) };
+
+        chrdev_reg.as_mut().register::<CompletionRust>()?;
 
         Ok(CompletionDev { _dev: chrdev_reg })
     }
